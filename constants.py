819204da60edb39913831594c17dd6832ee00db5
gigoro33 <73367547+gigoro33@users.noreply.github.com>
2025-08-04 12:36:11 -0500

    Update version, add country selection, and localize strings for Argentina and Colombia (#1)

--- constants.py
+++ constants.py
@@ -1,11 +1,19 @@
 from enum import Enum
+import xbmcaddon
 
 class Categoria(Enum):
     PELICULAS = "peliculas"
     SERIES = "series"
     INFANTIL = "infantil"
 
-BASE_URL = "https://play.mercadolibre.com.ar"
+addon = xbmcaddon.Addon()
+country = addon.getSetting('country')
+
+if country == "1": # Colombia
+    BASE_URL = "https://play.mercadolibre.com.co"
+else:  # Default to Argentina
+    BASE_URL = "https://play.mercadolibre.com.ar"
+
 REFERER_URL = f"{BASE_URL}/"
 API_URL = f"{BASE_URL}/api/"
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36'
